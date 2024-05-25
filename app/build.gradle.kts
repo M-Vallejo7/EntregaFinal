3a08d642e01c6432fa61107b8812eb0cbb512041
M-Vallejo7 <giraldomiguel21@gmail.com>
2024-05-25 10:19:44 -0500

    Next Step

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Mis Implementaciones (5)
     implementation("com.airbnb.android:lottie:6.4.0")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
