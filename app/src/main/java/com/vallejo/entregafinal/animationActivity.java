5c91408ca94c919bfea349c12a2f7eb8e3e48098
M-Vallejo7 <giraldomiguel21@gmail.com>
2024-05-24 23:21:52 -0500

    First Step

---
+++ app/src/main/java/com/vallejo/entregafinal/animationActivity.java
@@ -0,0 +1,23 @@
+package com.vallejo.entregafinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class animationActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(animationActivity.this, loginActivity.class));
+                finish();
+            }
+        }, 2000);
+
+    }
+}
---
+++ app/src/main/java/com/vallejo/entregafinal/loginActivity.java
@@ -0,0 +1,4 @@
+package com.vallejo.entregafinal;
+
+public class loginActivity {
+}
