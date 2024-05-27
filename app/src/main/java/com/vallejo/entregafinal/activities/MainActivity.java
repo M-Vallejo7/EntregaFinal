423780c81d511a09e99a09f50850d3ef6d2f9f7c
M-Vallejo7 <giraldomiguel21@gmail.com>
2024-05-26 21:04:15 -0500

    Sexta Etapa
    Añadir nuevo chat con usuarios ya registrados. Parte visual del MainActivity, función de añadir

--- app/src/main/java/com/vallejo/entregafinal/activities/MainActivity.java
+++ app/src/main/java/com/vallejo/entregafinal/activities/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners(){
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
     }
 
     private void loadUserDetails(){
@@ -64,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
                         preferenceManager.getString(Constants.KEY_USER_ID)
                 );
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
+                // Alerta que indica que el token fue actualizado para el usuario
+                // .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
