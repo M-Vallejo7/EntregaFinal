5c6a425f9a1160014354189b4021daf6ca60d5c9
M-Vallejo7 <giraldomiguel21@gmail.com>
2024-05-27 12:22:18 -0500

    12a Etapa
    Implementacion de notificaciones Push

--- app/src/main/java/com/vallejo/entregafinal/firebase/MessagingService.java
+++ app/src/main/java/com/vallejo/entregafinal/firebase/MessagingService.java
@@ -1,9 +1,26 @@
 package com.vallejo.entregafinal.firebase;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.vallejo.entregafinal.R;
+import com.vallejo.entregafinal.activities.ChatActivity;
+import com.vallejo.entregafinal.models.User;
+import com.vallejo.entregafinal.utilities.Constants;
+
+import java.util.Random;
 
 public class MessagingService extends FirebaseMessagingService {
 
@@ -20,5 +37,53 @@ public class MessagingService extends FirebaseMessagingService {
         super.onMessageReceived(remoteMessage);
         // Aqui se visualiza en Logcat un mensaje remoto entrante desde firebase
         //Log.d("FCM", "Message: " + remoteMessage.getNotification().getBody());
+        User user = new User();
+        user.id = remoteMessage.getData().get(Constants.KEY_USER_ID);
+        user.name = remoteMessage.getData().get(Constants.KEY_NAME);
+        user.token = remoteMessage.getData().get(Constants.KEY_FCM_TOKEN);
+
+        int notificationId = new Random().nextInt();
+        String channelId = "chat_message";
+
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.putExtra(Constants.KEY_USER, user);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId);
+        builder.setSmallIcon(R.drawable.ic_notification);
+        builder.setContentTitle(user.name);
+        builder.setContentText(remoteMessage.getData().get(Constants.KEY_MESSAGE));
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(
+                remoteMessage.getData().get(Constants.KEY_MESSAGE)
+        ));
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence channelName = "Chat Message";
+            String channelDescription = "This notification channel is used for chat message notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+            channel.setDescription(channelDescription);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        // Permisos para el notificationManagerCompat
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        // Alt + Enter (según la alerta, era para añadirle los permisos, puesto que la linea de abajo tenía error)
+        notificationManagerCompat.notify(notificationId, builder.build());
     }
 }
--- app/src/main/java/com/vallejo/entregafinal/utilities/Constants.java
+++ app/src/main/java/com/vallejo/entregafinal/utilities/Constants.java
@@ -1,5 +1,7 @@
 package com.vallejo.entregafinal.utilities;
 
+import java.util.HashMap;
+
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_NAME = "name";
@@ -23,5 +25,24 @@ public class Constants {
     public static final String KEY_RECEIVER_IMAGE = "receiverImage";
     public static final String KEY_LAST_MESSAGE = "lastMessage";
     public static final String KEY_AVAILABILITY = "availability";
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
 
+    public static HashMap<String, String> remoteMsgHeaders = null;
+    public static HashMap<String, String> getRemoteMsgHeaders(){
+        if(remoteMsgHeaders == null){
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=BG3m2_YLYGoKVjt2ghJopMRrG5BgeeErlC48DImp1awkoaHQg56pbr_OsgXcyv1r3G9xNHG-IZAqAlIz1blNSDc"
+            );
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_CONTENT_TYPE,
+                    "aplication/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
