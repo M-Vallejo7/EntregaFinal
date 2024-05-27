e5bed470844bba0d578a01f0f7c862f30467e0e8
M-Vallejo7 <giraldomiguel21@gmail.com>
2024-05-26 22:19:54 -0500

    Séptima Parte
    Diseño de la pantalla de chat (mensajes)

---
+++ app/src/main/java/com/vallejo/entregafinal/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.vallejo.entregafinal.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vallejo.entregafinal.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
